31e90af2c7b5f23fef89cf1497cad5f5fdb0d3c4
zhujianan <2494911084@qq.com>
2020-12-10 14:15:06 +0800

    修复pc首页bug

--- resources/views/p/arts/1.blade.php
+++ resources/views/p/arts/1.blade.php
@@ -1,5 +1,9 @@
+@if (count($mr_cate->arts)>0)
+
 <div class="cttitle explaintl">{{ ($mr_cate->arts)[0]['title'] }}</div>
 <div class="articlect" id="zoomcon">
     {!! ($mr_cate->arts)[0]['content'] !!}
 
 </div>
+
+@endif
